6b565785deba09dcbec4d32472a4ce30dae614e9
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-10 11:28:46 +0500

    sum_odd.py

--- sum_odd.py
+++ sum_odd.py
@@ -5,3 +5,15 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the odd digits in the variable "var_int".
+var_int = 1835
+x1 = var_int%10
+x2 = var_int%100//10
+x3 = var_int%1000//100
+x4 = var_int//1000
+y1 = (x1)%2*x1
+y2 = (x2)%2*x2
+y3 = (x3)%2*x3
+y4 = (x4)%2*x4
+
+sum_even = y1 + y2 + y3 + y4
+print(sum_even)
