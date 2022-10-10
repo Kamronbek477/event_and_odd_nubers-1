aee13df195415497b9e73b06e05998b2c5fda3be
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-10 10:23:50 +0500

    number_odd.py

--- number_odd.py
+++ number_odd.py
@@ -3,3 +3,15 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of odd digits in the variable "var_int".
+a = 2468
+x1 = a%10
+x2 = a%100//10
+x3 = a%1000//100
+x4 = a//1000
+y1 = (x1+1)%2
+y2 = (x2+1)%2
+y3 = (x3+1)%2
+y4 = (x4+1)%2
+
+var_int = 4 - (y1 + y2 + y3 + y4)
+print(var_int)
