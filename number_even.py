73f119f453073afd14fd7f5d214b3c45bf63bcc8
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-10 10:54:07 +0500

    number_even.py

--- number_even.py
+++ number_even.py
@@ -3,15 +3,15 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of even digits in the variable "var_int".
-a = 1246
-x1 = a%10
-x2 = a%100//10
-x3 = a%1000//100
-x4 = a//1000
+var_int = 1221
+x1 = var_int%10
+x2 = var_int%100//10
+x3 = var_int%1000//100
+x4 = var_int//1000
 y1 = (x1+1)%2
 y2 = (x2+1)%2
 y3 = (x3+1)%2
 y4 = (x4+1)%2
 
-var_int = y1 + y2 + y3 + y4
-print(var_int)
+a = y1 + y2 + y3 + y4
+print(a)
